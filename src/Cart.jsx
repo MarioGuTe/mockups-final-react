6e39a6cd603cc4afe4f953f4bfd326d3910b7b0b
MarioGuTe <mariogutierreztello@gmail.com>
2023-03-30 18:18:20 -0300

    update cart

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -4,54 +4,50 @@ import styles from "./Cart.module.css";
 
 function Cart() {
   return (
-    <>
-      <nav>hola</nav>
-      <div className={styles.cartMainContainer}>
-        <div className={styles.itemsSection}>
-          <h1>Carro de compras</h1>
-          <div className={styles.cardProduct}>
-            <figure>
-              <img src={zanzibar} alt="" />
-            </figure>
-            <div className={styles.cardDescription}>
-              <h2>Planta decorativa</h2>
-              <p>$ 9990</p>
-            </div>
-            <div className={styles.productQty}>
-              <button className={styles.sub}>-</button>
-              <p>1</p>
-              <button className={styles.add}>+</button>
-            </div>
+    <div className={styles.cartMainContainer}>
+      <div className={styles.itemsSection}>
+        <h1>Carro de compras</h1>
+        <div className={styles.cardProduct}>
+          <figure>
+            <img src={zanzibar} alt="" />
+          </figure>
+          <div className={styles.cardDescription}>
+            <h2>Planta decorativa</h2>
+            <p>$ 9990</p>
           </div>
-          <div className={styles.cardProduct}>
-            <figure>
-              <img src={zanzibar} alt="" />
-            </figure>
-            <div className={styles.cardDescription}>
-              <h2>Planta decorativa</h2>
-              <p>$ 9990</p>
-            </div>
-            <div className={styles.productQty}>
-              <button className={styles.sub}>-</button>
-              <p>1</p>
-              <button className={styles.add}>+</button>
-            </div>
+          <div className={styles.productQty}>
+            <button className={styles.sub}>-</button>
+            <p>1</p>
+            <button className={styles.add}>+</button>
           </div>
         </div>
-        <div className={styles.checkoutContainer}>
-          <div className={styles.checkoutCard}>
-            <div className={styles.checkoutText}>
-              <p>Total</p>
-              <p>$ 19980</p>
-            </div>
-            <div className={styles.btnContainer}>
-              <button>Checkout</button>
-            </div>
+        <div className={styles.cardProduct}>
+          <figure>
+            <img src={zanzibar} alt="" />
+          </figure>
+          <div className={styles.cardDescription}>
+            <h2>Planta decorativa</h2>
+            <p>$ 9990</p>
+          </div>
+          <div className={styles.productQty}>
+            <button className={styles.sub}>-</button>
+            <p>1</p>
+            <button className={styles.add}>+</button>
           </div>
         </div>
       </div>
-      <footer>hola</footer>
-    </>
+      <div className={styles.checkoutContainer}>
+        <div className={styles.checkoutCard}>
+          <div className={styles.checkoutText}>
+            <p>Total</p>
+            <p>$ 19980</p>
+          </div>
+          <div className={styles.btnContainer}>
+            <button>Checkout</button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
